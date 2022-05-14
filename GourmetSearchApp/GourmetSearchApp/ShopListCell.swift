889691d31739cdd98c8e7db8f3fa3a542fb9d8ea
藤原和矢 <kazuya.fujiwara@axas-japan.co.jp>
2022-05-14 14:57:01 +0900

    220514AM

---
+++ GourmetSearchApp/GourmetSearchApp/ShopListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShopListCell.swift
+//  
+//
+//  Created by AXLT0220-AP on 2022/05/09.
+//
+
+import UIKit
+
+class ShopListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
